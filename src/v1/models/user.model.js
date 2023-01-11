a4fa3babb5f1825a03a81c94872b09ad2e205c2f
Phan Tấn Lộc <ginga550504@gmail.com>
2023-01-11 22:49:24 +0700

    init environment

---
+++ src/v1/models/user.model.js
@@ -0,0 +1,47 @@
+const { Schema, model } = require("mongoose");
+
+const UserSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      index: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    full_name: {
+      type: String,
+      default: "",
+      index: true,
+    },
+    key_change_password: {
+      type: String,
+      default: "",
+    },
+    roleId: {
+      type: Schema.Types.ObjectId,
+      ref: "Roles",
+      required: true,
+    },
+    image: {
+      type: String,
+      default: "",
+    },
+    access_token: {
+      type: String,
+      default: "",
+    },
+    is_delete: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    collection: "users",
+    timestamps: true,
+  }
+);
+
+module.exports._User = model("users", UserSchema);
